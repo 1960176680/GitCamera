309c6f40e2156b1ae6f53e2916b241aded5a0176
Sun <1960176680@qq.com>
2017-09-25 17:32:19 +0800

    相机相关
    相机相关

---
+++ 相机相册/photo/photopickerdemo3/src/main/java/com/xy/administrator/photopickerdemo3/MainActivity.java
@@ -0,0 +1,60 @@
+package com.xy.administrator.photopickerdemo3;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.OrientationHelper;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.util.Log;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import me.iwf.photopicker.PhotoPickerActivity;
+import me.iwf.photopicker.utils.PhotoPickerIntent;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE =1 ;
+    ArrayList<String> selectedPhotos = new ArrayList<>();
+    RecyclerView recyclerView;
+    PhotoAdapter photoAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        photoAdapter = new PhotoAdapter(this, selectedPhotos);
+        recyclerView.setLayoutManager(new StaggeredGridLayoutManager(4, OrientationHelper.VERTICAL));
+        recyclerView.setAdapter(photoAdapter);
+    }
+    public void click(View view){
+        PhotoPickerIntent intent = new PhotoPickerIntent(MainActivity.this);
+        intent.setPhotoCount(5);
+        intent.setShowCamera(false);
+        startActivityForResult(intent, REQUEST_CODE);
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        List<String> photos = null;
+        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE) {
+            if (data != null) {
+                photos = data.getStringArrayListExtra(PhotoPickerActivity.KEY_SELECTED_PHOTOS);
+                Log.i("tag","================list集合--->"+photos.toString());
+            }
+            selectedPhotos.clear();
+
+            if (photos != null) {
+
+                selectedPhotos.addAll(photos);
+            }
+            photoAdapter.notifyDataSetChanged();
+        }
+    }
+    public void previewPhoto(Intent intent) {
+        startActivityForResult(intent, REQUEST_CODE);
+    }
+}
